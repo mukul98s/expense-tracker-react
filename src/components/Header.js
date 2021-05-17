b3bc476ad0161a1a2b6c25d9ac33202b2a0dbc2a
mukul98s <mymukul112@gmail.com>
2021-05-17 17:47:17 +0530

    Total Balance is Calculated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,11 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 function Header() {
+  const { transactions } = useContext(GlobalContext);
+
+  const money = transactions
+    .map((transaction) => transaction.amount)
+    .reduce((a, b) => a + b, 0);
   return (
     <div className="header">
       <div className="header__title">
@@ -10,7 +15,8 @@ function Header() {
       <div className="header__balance">
         <h3>Your Balance</h3>
         <h2>
-          <span className="money">Rs.</span>480
+          <span className="money">Rs.</span>
+          {money}
         </h2>
       </div>
     </div>
