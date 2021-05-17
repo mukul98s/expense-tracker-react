2b83129d913585511419d0f900c3674a7ca8e02c
mukul98s <mymukul112@gmail.com>
2021-05-17 16:50:39 +0530

    added componets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import AddTransaction from "./components/AddTransaction";
+import Expense from "./components/Expense";
+import Header from "./components/Header";
+import TransactionList from "./components/TransactionList";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <Expense />
+      <TransactionList />
+      <AddTransaction />
+    </GlobalProvider>
   );
 }
 
