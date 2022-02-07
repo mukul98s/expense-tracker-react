a77a505747544fdaa4caf4d51a93d816bb859600
Mukul Sharma <mymukul112@gmail.com>
2022-02-07 18:02:02 +0530

    migrate to tsx
    - defined types on params

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -2,18 +2,24 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import Transaction from "./Transaction";
 
-function TransactionList() {
+interface transaction {
+  amount: number;
+  type: string;
+  id: number;
+}
+
+const TransactionList: React.FC = () => {
   const { transactions } = useContext(GlobalContext);
   return (
     <div className="transactionList">
       <h3>History</h3>
       <div className="transactionList__history">
-        {transactions.map((transaction) => {
+        {transactions.map((transaction: transaction) => {
           return <Transaction transaction={transaction} key={transaction.id} />;
         })}
       </div>
     </div>
   );
-}
+};
 
 export default TransactionList;
